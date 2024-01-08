6e76117a2666dfe4cfbe35fb363564012d4d04b4
Taylor Monacelli <taylormonacelli@gmail.com>
2024-01-08 08:31:07 -0800

    working

--- main.py
+++ main.py
@@ -1,28 +1,62 @@
+import csv
 import io
+import re
 
 import pandas
 
-with open("data.csv", "r") as file:
+in1 = "data.csv"
+out1 = "normalized_data.csv"
+
+header = "TYPE,DATE,START TIME,END TIME,IMPORT (KWh),EXPORT (KWh),NOTES"
+
+
+def assert_file_contains_expected_line(file_path, expected_line):
+    regex_pattern = re.compile(rf"^{re.escape(expected_line)}$")
+
+    with open(file_path, "r") as file:
+        for line_number, line in enumerate(file, start=1):
+            if regex_pattern.match(line.strip()):
+                break
+        else:
+            raise AssertionError(
+                "Expected line not found in the file "
+                f"'{file_path}' at line {line_number}"
+            )
+
+
+try:
+    assert_file_contains_expected_line(in1, header)
+except AssertionError as e:
+    print(f"AssertionError: {e}")
+    print(f"Expected line: {header}")
+    raise e
+
+with open(in1, "r", newline="") as infile, open(out1, "w", newline="") as outfile:
+    reader = csv.reader(infile)
+    writer = csv.writer(outfile)
+
+    for row in reader:
+        if len(row) == 6:
+            row.append("")
+        writer.writerow(row)
+
+
+with open(out1, "r") as file:
     for line in file:
-        if line.startswith(
-            "TYPE,DATE,START TIME,END TIME,IMPORT (KWh),EXPORT (KWh),NOTES"
-        ):
+        if line.startswith(header):
             break
 
     data = file.read()
 
+data = header + "\n" + data
+
+
+header_names = header.split(",")
+
 df = pandas.read_csv(
     io.StringIO(data),
     header=None,
-    names=[
-        "TYPE",
-        "DATE",
-        "START TIME",
-        "END TIME",
-        "IMPORT (KWh)",
-        "EXPORT (KWh)",
-        "NOTES",
-    ],
+    names=header_names,
 )
 
 df = pandas.read_csv(io.StringIO(data), sep=",")
