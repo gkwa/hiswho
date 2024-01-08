4e9c6acb5ad6e942beac215fb85faec7baa92c8f
Taylor Monacelli <taylormonacelli@gmail.com>
2024-01-08 07:52:43 -0800

    failing

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-import pandas
 import io
+
+import pandas
 
 with open("data.csv", "r") as file:
     for line in file:
-        if line.startswith("TYPE,DATE,START TIME,END TIME,IMPORT (KWh),EXPORT (KWh),NOTES"):
+        if line.startswith(
+            "TYPE,DATE,START TIME,END TIME,IMPORT (KWh),EXPORT (KWh),NOTES"
+        ):
             break
 
     data = file.read()
+
+df = pandas.read_csv(
+    io.StringIO(data),
+    header=None,
+    names=[
+        "TYPE",
+        "DATE",
+        "START TIME",
+        "END TIME",
+        "IMPORT (KWh)",
+        "EXPORT (KWh)",
+        "NOTES",
+    ],
+)
 
 df = pandas.read_csv(io.StringIO(data), sep=",")
 df["datetime"] = pandas.to_datetime(df["DATE"] + " " + df["START TIME"])
