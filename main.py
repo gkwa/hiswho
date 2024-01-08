a07f56c10f9a90f0e73eb27e8a799b070e134510
Taylor Monacelli <taylormonacelli@gmail.com>
2024-01-08 07:41:57 -0800

    Failing to parse csv since column count varies

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas
+import io
+
+with open("data.csv", "r") as file:
+    for line in file:
+        if line.startswith("TYPE,DATE,START TIME,END TIME,IMPORT (KWh),EXPORT (KWh),NOTES"):
+            break
+
+    data = file.read()
+
+df = pandas.read_csv(io.StringIO(data), sep=",")
+df["datetime"] = pandas.to_datetime(df["DATE"] + " " + df["START TIME"])
+df.set_index("datetime", inplace=True)
+rolling_avg = df["IMPORT (KWh)"].rolling(window=4).mean()
+df["rolling_avg"] = rolling_avg
+
+print(df[["DATE", "START TIME", "IMPORT (KWh)", "rolling_avg"]])
