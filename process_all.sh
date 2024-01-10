939d6b9bce7546619d422ae5c82512c5dd438e10
Taylor Monacelli <taylormonacelli@gmail.com>
2024-01-10 11:59:57 -0800

    less noise please
    less noise please
    
    less noise please

--- process_all.sh
+++ process_all.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/evn bash
 
-rm -f all.json
 rm -f all.jsonl
 
 find /Users/mtm/pdev/taylormonacelli/eachload/data -type f -name "*scl_electric_usage_interval_data*.csv" -exec sh -c '
     for file do
-        python /Users/mtm/pdev/taylormonacelli/hiswho/main.py --inpath "${file}"
+        python main.py --inpath "${file}"
     done
 ' sh {} +
 cat *.jsonl | sort >all.jsonl
 jq -s '.' all.jsonl >all.json
-
 rm -f all.jsonl
